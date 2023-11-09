2eea3e47404a97f5414a05a7a20b71fdc6a65fd9
adeyemimike123 <ayobami732000@gmail.com>
2023-11-09 20:26:02 +0100

    this is the initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './components/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <div>
+      <Router>
+     <Home/>
+     </Router>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const  = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+
+
+    </div>
+  )
+}
