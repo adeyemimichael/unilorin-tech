ec55754599f0aac1925223dc0dee6ebfe17dafc5
adeyemimike123 <ayobami732000@gmail.com>
2023-11-23 15:33:33 +0100

    i added some slick slider session

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,9 +1,17 @@
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import image0 from "../assets/images/anu.webp";
+import image1 from "../assets/images/att1.webp";
+import image2 from "../assets/images/att2-45ef2d9f.webp";
+import image3 from "../assets/images/att4.webp";
+import image4 from "../assets/images/DSC_3927.jpg";
+import image5 from "../assets/images/DSC_3996.jpg";
+import image6 from "../assets/images/atted.jpg";
 const Highlight = () => {
   const settings = {
     arrows: true,
+    infinite: true,
       dots: false,
       slidesToShow: 3,
       autoplay:false,
@@ -25,43 +33,47 @@ const Highlight = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
+          initialSlide: 2,
+          infinite: true
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          infinite: true
         }
       }
     ]
   };
   return (
     <div>
-      <div className="  md:w-screen md:h-fit p-20 h-fit  ">
- <h2 className='font-brico md:font-semibold font-normal text-center md:text-[72px] text-[32px] p-8'> Highlights for UTS 2022</h2>
-  <div className="w-full ">
+      <div className="  md:w-screen md:h-fit  h-fit  ">
+ <h2 className='font-brico md:font-semibold font-normal text-center md:text-[72px] text-[32px] p-8 w-screen '> Highlights for UTS 2023</h2>
+  <div className="md:w-full  w-screen h-fit relative  md:left-0  left-0  ">
  
       <Slider {...settings}>
-        <div className=" flex justify-center items-center text-white text-center slider bg-[#FFD100] md:w-[200px] md:h-[600px] w-[200px] h-[397px]   ">
-
-          <h3>1</h3>
+        <div className=" flex justify-center items-center overflow-hidden text-white text-center slider bg-[#FFD100] md:w-[200px] md:h-[300px]  w-[300px] h-[260px]   ">
+<img src={image0} alt="" className="w-full h-full object-fit " />
         </div>
-        <div className=" flex justify-center items-center slider text-white text-center bg-[#FFD100]  md:w-[200px] md:h-[600px] w-[200px] h-[397px] mx-10">
-          <h3>2</h3>
+        <div className=" flex justify-center items-center slider text-white text-center bg-[#FFD100]  md:w-[200px] md:h-[300px]  w-[300px] h-[260px] mx-10">
+        <img src={image6} alt="" className="w-full h-full object-fit " />
         </div>
-        <div className=" flex justify-center items-center slider text-white text-center  bg-[#FFD100] md:w-[200px] md:h-[600px] w-[200px] h-[397px] ml-10">
-          <h3>3</h3>
+        <div className=" flex justify-center items-center slider text-white text-center  bg-[#FFD100] md:w-[200px] md:h-[300px]  w-[300px] h-[260px] ml-10">
+        <img src={image1} alt="" className="w-full h-full object-fit " />
         </div>
-        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[600px] w-[200px] h-[397px]  " >
-          <h3>4</h3>
+        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[300px] w-[300px] h-[260px]  " >
+        <img src={image2} alt="" className="w-full h-full object-fit " />
         </div>
-        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[600px] w-[200px] h-[397px] ">
-          <h3>5</h3>
+        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[300px]  w-[300px] h-[260px] ">
+        <img src={image4} alt="" className="w-full h-full object-fit " />
         </div>
-        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[600px] w-[200px] h-[397px] ">
-          <h3>6</h3>
+        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[300px] w-[300px] h-[260px] ">
+        <img src={image5} alt="" className="w-full h-full object-fit " />
+        </div>
+        <div className=" flex justify-center items-center text-white text-center slider  bg-[#FFD100]  md:w-[200px] md:h-[300px] w-[300px] h-[260px] ">
+        <img src={image3} alt="" className="w-full h-full object-fit " />
         </div>
       </Slider>
       </div>
